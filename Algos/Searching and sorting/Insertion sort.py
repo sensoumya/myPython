a352a438699348ee957bd530ad818449777f7ae0
sensoum <ssen.mailbox@gmail.com>
2019-01-07 13:42:38 +0530

    add

---
+++ Algos/Searching and sorting/Insertion sort.py
@@ -0,0 +1,17 @@
+'''
+The selection sort improves on the bubble sort by making only one exchange for every pass through the list. In order to do this, a selection sort looks for the largest value as it makes a pass and, after completing the pass, places it in the proper location. As with a bubble sort, after the first pass, the largest item is in the correct place. After the second pass, the next largest is in place. This process continues and requires n−1 passes to sort n items, since the final item must be in place after the (n−1) st pass.
+'''
+import random
+
+
+def ins_sort(arr):
+    length = len(arr)
+    for i in range(length - 1):
+        if arr[i] != min(arr[i:]):
+            j = arr.index(min(arr[i:]))
+            arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+
+arr = random.sample(range(1, 100), 20)
+print(sel_sort(arr))
