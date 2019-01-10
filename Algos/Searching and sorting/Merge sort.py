2eba5ebcaae0e5092ca4e03ef40a6ec003977e26
Soumya Sen <ssen.mailbox@gmail.com>
2019-01-10 18:06:57 +0530

    Add files via upload

--- Algos/Searching and sorting/Merge sort.py
+++ Algos/Searching and sorting/Merge sort.py
@@ -1,15 +1,39 @@
 import random
 
 
-def ins_sort(arr):
-    for i in range(1, len(arr)):
-        j = i
-        while j > 0 and arr[j] < arr[:j][-1]:
-            arr[j - 1], arr[j] = arr[j], arr[j - 1]
-            j -= 1
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        l_arr = arr[:mid]
+        r_arr = arr[mid:]
+
+        merge_sort(l_arr)
+        merge_sort(r_arr)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(l_arr) and j < len(r_arr):
+            if l_arr[i] < r_arr[j]:
+                arr[k] = l_arr[i]
+                i += 1
+                k += 1
+            else:
+                arr[k] = r_arr[j]
+                j += 1
+                k += 1
+
+        while i < len(l_arr):
+            arr[k] = l_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(r_arr):
+            arr[k] = r_arr[j]
+            j += 1
+            k += 1
     return arr
 
 
-arr = random.sample(range(1, 100), 20)
-print(arr)
-print(ins_sort(arr))
+# arr = random.sample(range(1, 100), 20)
+# print(arr)
+print(merge_sort([42, 4, 6, 97, 29]))
--- Algos/Searching and sorting/Quick sort.py
+++ Algos/Searching and sorting/Quick sort.py
@@ -1,15 +1,40 @@
 import random
 
 
-def ins_sort(arr):
-    for i in range(1, len(arr)):
-        j = i
-        while j > 0 and arr[j] < arr[:j][-1]:
-            arr[j - 1], arr[j] = arr[j], arr[j - 1]
-            j -= 1
+def quick_sort(arr):
+    quick_helper(arr, 0, len(arr) - 1)
     return arr
 
 
-arr = random.sample(range(1, 100), 20)
-print(arr)
-print(ins_sort(arr))
+def quick_helper(arr, first, last):
+    if first < last:
+        split = partition(arr, first, last)
+        quick_helper(arr, first, split - 1)
+        quick_helper(arr, split + 1, last)
+
+
+def partition(arr, first, last):
+    pivot = arr[first]
+    left = first + 1
+    right = last
+    done = False
+    while not done:
+        print(arr)
+        while left <= right and arr[left] < pivot:
+            left += 1
+        while left <= right and arr[right] > pivot:
+            right -= 1
+        if left > right:
+            done = True
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+
+    print(arr)
+    arr[first], arr[right] = arr[right], arr[first]
+    print(arr)
+    return right
+
+
+# arr = random.sample(range(1, 100), 20)
+# print(arr)
+print(quick_sort([42, 4, 6, 97, 29]))
