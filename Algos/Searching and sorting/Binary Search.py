f128a37cf205c5ebce4b0864cd9038349ee5581f
sensoum <ssen.mailbox@gmail.com>
2019-01-13 16:35:53 +0530

    Graph algorithms

---
+++ Algos/Searching and sorting/Binary Search.py
@@ -0,0 +1,21 @@
+import random
+
+
+def shellSort(arr):
+    n = len(arr)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+
+
+arr = random.sample(range(1, 100), 20)
+print(arr)
+print(shellSort(arr))
